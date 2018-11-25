2b1aa9ae340e5417b123ef3c0eda0040afd3d7a5
dahoony <crysis1@naver.com>
2018-11-25 20:47:43 +0900

    tab 구성 및 화면 수정

--- getting/src/components/personComponents/Person.js
+++ getting/src/components/personComponents/Person.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
-import Profile from './Profile';
+import Profile from "./Profile";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { Image } from "react-bootstrap";
+import "react-tabs/style/react-tabs.css";
+import "./Person.css";
 
 class Person extends Component {
   constructor(props) {
     super(props);
   }
-  render(){
-      return (
-          <div>
-              <Profile></Profile>
-          </div>
-      )
+
+  render() {
+    return (
+      <div>
+        <div className="person_header">
+          <a href="#">
+            <img src="Logo.png" className="logo_img" />
+          </a>
+        </div>
+        <Tabs>
+          <TabList>
+            <Tab>Title 1</Tab>
+            <Tab>Title 2</Tab>
+            <button>+</button>
+          </TabList>
+          <TabPanel>
+            <div className="pat1">
+              <div className="profile">
+                <img src="./images/pat1.png" className="profile-img" />
+              </div>
+              <h2>Any content 1</h2>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
+    );
   }
 }
 
