d370f79d6b15057634cbfeedf6a3ee7efcb85d6f
sanghoon <dbql110481@naver.com>
2018-11-27 17:04:03 +0900

    login page

--- getting/src/components/App.js
+++ getting/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Person from './personComponents/Person';
+import Login from './mainComponents/Login';
 
 class App extends Component {
     render() {
       return (
         <div className="ui container">
-            <Person></Person>
+            <Login></Login>
         </div>
       );
     }
