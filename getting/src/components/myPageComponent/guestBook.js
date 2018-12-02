55402ef536cd0d7e3520df13cc4abf0c64be4cfa
DESKTOP-AV87NGJ\Yang's <you@example.com>
2018-12-02 16:38:57 +0900

    거의 거의 다되감

--- getting/src/components/myPageComponent/guestBook.js
+++ getting/src/components/myPageComponent/guestBook.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button } from "reactstrap";
 
 export default class guestBook extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="visitors">
+        <div className="comments">
+          <div className="headComment">
+            <img src="./person_images/photos/animal-prints.png" className="foot" alt="" />
+            <strong>양초다</strong>
+          </div>
+          <hr className="hrs" />
+          <div className="bodyComment">
+            <img src="./person_images/photos/profileTest.jpg" className="proImg" alt="" />
+            사진들 잘 보고갑니다~~
+            </div>
+        </div>
       </div>
     )
   }
--- getting/src/components/myPageComponent/profile.js
+++ getting/src/components/myPageComponent/profile.js
@@ -1,36 +1,46 @@
 import React, { Component } from "react";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
-import './profile.css';
+import "./profile.css";
 
-export default class profile extends Component {
+class Profile extends Component {
+  likes = [];
+
+  constructor(props) {
+    super(props);
+    this.likes = props.likes.map(like => {
+      return (
+        <div className="likeInfo">
+          <img src={like.url} className="likeImg"></img>
+          <strong>{like.name}</strong>({like.gender})
+        </div>
+      );
+    });
+  }
 
   render() {
     return (
-      <Tabs>
-        <TabList>
-          <Tab>my pet 1</Tab>
-          <Tab>my pet 2</Tab>
-        </TabList>
-
-        <TabPanel>
-          <div className="body">
-            <div className="profile">
-              <div className="profileImg">
-                <img src="./images/pat1.png" className="profile-image" alt="" />
-              </div>
-              <div className="profileInfo">
-                <p className="p_txt">견종 : 포메라니안</p>
-                <p className="p_txt">나이 : 2살</p>
-                <p className="p_txt">성별 : 여</p>
-              </div>
+      <div className="profile">
+        <div className="profileImg">
+          <img src="./images/pat1.png" className="profile-img" />
+        </div>
+        <div className="profileInfo">
+          <div className="profileInfoes">
+            <p className="pStyle">견종 : 포메라니안</p>
+            <p className="pStyle">나이 : 2살</p>
+            <p className="pStyle">성별 : 여</p>
+          </div>
+          <hr className="hrs" />
+          <div className="likes">
+            <div className="boneImg">
+              <img src="./person_images/bone.png" className="bone" />
+            </div>
+            <div className="likeInfoes">
+              {this.likes}
             </div>
           </div>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-      </Tabs>
+        </div>
+      </div>
     );
   }
 }
+
+export default Profile;
