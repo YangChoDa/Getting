b8b1b8e2d68a4ceeb1f5050d3574ee1ac87ceaf9
sanghoon <dbql110481@naver.com>
2018-12-03 11:32:01 +0900

    main

--- getting/src/components/App.js
+++ getting/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { MyPage, Person, Login } from "./index";
+import { MyPage, Person, Login, Main } from "./index";
 import PageTemplate  from './common/PageTemplate/index';
 
 import "./App.css";
@@ -9,7 +9,8 @@ const App = () => {
   return (
     <div className="main">
       <Switch>
-        <Route exact path="/" component={PageTemplate}/>
+        {/* <Route exact path="/" component={PageTemplate}/> */}
+        <Route exact path="/" component={Main}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/person" component={Person}/>
         <Route exact path="/mypage" component={MyPage}/>
--- getting/src/components/index.js
+++ getting/src/components/index.js
@@ -1,4 +1,4 @@
 export {default as MyPage} from './myPageComponent/MyPage';
-
+export {default as Main} from './mainComponents/Main';
 export {default as Person} from './personComponents/Person';
 export {default as Login} from './mainComponents/Login';
